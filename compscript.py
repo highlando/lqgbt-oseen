f47d9b3f204046c85aa72da5aa2ef94365ca52e8
highlando <jnhlnd@gmail.com>
2014-08-12 19:33:00 +0200

    update num ex

--- compscript.py
+++ compscript.py
@@ -4,7 +4,7 @@ import datetime
 
 # to compute stabilizing initial values for higher Re numbers
 # relist = [None, 5.0e1, 1.0e2, 1.5e2, 2.0e2, 2.5e2, 3.0e2]  # , 3.5e2, 4.0e2]
-relist = [None, 2.5e2]  # , 3.5e2, 4.0e2]
+relist = [None, 2.0e2]  # , 3.5e2, 4.0e2]
 
 # mesh parameter for the cylinder meshes
 cyldim = 3
@@ -13,7 +13,7 @@ trunclist = [1e-3]  # , 1e-3, 1e-2, 1e-1, 1e-0]
 # dimension of in and output spaces
 NU, NY = 3, 3
 # to what extend we perturb the initial value
-perturbpara = 1e-3
+perturbpara = 1e-6
 # closed loop def
 closed_loop = 'red_output_fb'
 # number of time steps -- also define the lag in the control application
