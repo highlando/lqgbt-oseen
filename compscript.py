a4bacc74f38366498e8cd9a6873002b2076405b6
highlando <jnhlnd@gmail.com>
2018-02-21 14:25:43 +0800

    working!

--- compscript.py
+++ compscript.py
@@ -10,8 +10,8 @@ import getopt
 pymess = True
 pymess = False
 relist = [None, 5.0e1, 1.0e2, 1.15e2, 1.25e2, 1.35e2]  # , 1.45e2]
-max_re_only = True  # consider only the last Re for the simu
 max_re_only = False
+max_re_only = True  # consider only the last Re for the simu
 
 # the input regularization parameter
 gamma = 1.  # e5
@@ -36,11 +36,11 @@ robit = False
 robmrgnfac = 0.1
 # closed loop def
 closed_loop = 'full_state_fb'
+closed_loop = False
 closed_loop = None
 closed_loop = 'red_output_fb'
-closed_loop = False
 # number of time steps -- also define the lag in the control application
-scaletest = 0.6  # for 1. we simulate till 12.
+scaletest = 1  # 0.6  # for 1. we simulate till 12.
 t0, tE, baseNts = 0.0, scaletest*12.0, np.int(scaletest*1*2.4e3+1)
 
 # get command line input and overwrite standard paramters if necessary
