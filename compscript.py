a9bebf374fa194fd14a2461d08d09cdc88df73a9
highlando <jnhlnd@gmail.com>
2018-09-09 22:25:33 +0200

    fixing the docs

--- compscript.py
+++ compscript.py
@@ -23,14 +23,14 @@ bccontrol = True
 palpha = 1e-5  # parameter for the Robin penalization
 cyldim = 3
 # where to truncate the LQGBT characteristic values
-trunclist = [1e-1]  # , 1e-3, 1e-2, 1e-1, 1e-0]
+trunclist = [1e-3]  # , 1e-2, 1e-1, 1e-0]
 # dimension of in and output spaces
 NU, NY = 3, 3
 # to what extend we perturb the initial value
 perturbpara = 1e-3
 # whether we use a perturbed system
-trytofail = False
 trytofail = True
+trytofail = False
 ttf_npcrdstps = 3
 # whether to robustify the observer
 robit = True
@@ -44,9 +44,9 @@ closed_loop = 'redmod_sdre_fb'
 closed_loop = 'red_sdre_fb'
 closed_loop = False
 closed_loop = 'full_state_fb'
-closed_loop = 'red_output_fb'
 closed_loop = None
 closed_loop = 'hinf_red_output_fb'
+closed_loop = 'red_output_fb'
 # what inival
 whichinival = 'sstokes'  # steady state Stokes solution
 whichinival = 'sstokes++'  # a developed state starting from sstokes
