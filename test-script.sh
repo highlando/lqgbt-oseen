85ffba80475dec61527b9784c47688d5bab762d9
highlando <jnhlnd@gmail.com>
2019-01-03 16:45:57 +0100

    script for the tests

---
+++ test-script.sh
@@ -0,0 +1,12 @@
+# # need to specify
+#  * there: RE 
+#  * there: fb type
+#  * inival
+#  * npcrd
+#  * there: truncat
+#  * there: scaletest
+#  * cyldim
+#  * C
+#  * logfile <--- gonna use shell redirect
+
+python3 compscript.py --iniperturb=0.0001 --re=100 --closed_loop=4
