9b082625613c92f73c709dd71ec9e6e044c6234e
highlando <jnhlnd@gmail.com>
2019-02-14 10:36:53 +0800

    get the test script

---
+++ test-script.sh
@@ -0,0 +1,24 @@
+# # need to specify
+#  * there: RE 
+#  * there: fb type
+#  * inival: enforce ss+d via iniperturb
+#  * npcrd
+#  * there: truncat
+#  * there: scaletest
+#  * cyldim
+#  * C
+#  * logfile <--- gonna use shell redirect
+
+# python3 compscript.py --iniperturb=0.0001 --re=100 --closed_loop=4
+
+RE=100
+CYLDIM=3
+INIPERTURB=0.0001
+TRUNCAT=0.01
+FBTYPE=5
+NUMPICARDS=6
+SCALETEST=1.
+
+LOGFILE=logs/N${CYLDIM}re${RE}fbt${FBTYPE}nps${NUMPICARDS}trnc${TRUNCAT}sspd${INIPERTURB}st${SCALETEST}
+echo tail -f $LOGFILE
+python3 compscript.py --obsperturb=1 --iniperturb=${INIPERTURB} --re=${RE} --closed_loop=${FBTYPE}  --ttf_npcrdstps=${NUMPICARDS} --scaletest=${SCALETEST} --truncat=${TRUNCAT} --cyldim=${CYLDIM} >> $LOGFILE
