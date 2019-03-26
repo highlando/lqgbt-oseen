bcc8134c104613dd03f6a526ba3b957cc358d1f6
highlando <jnhlnd@gmail.com>
2019-03-26 08:03:48 +0100

    testing

--- tests/compscript.py
+++ tests/compscript.py
@@ -12,8 +12,8 @@ pymess = False
 relist = [None, 5e1, 7.5e1, 1e2, 1.2e2]
 relist = [5e1, 7.5e1]
 relist = [None, 5e1, 7.5e1, 9e1, 1e2]
-max_re_only = False
 max_re_only = True  # consider only the last Re for the simu
+max_re_only = False
 
 # the input regularization parameter
 gamma = 1e-0  # e5
@@ -49,8 +49,8 @@ closed_loop = 'red_updsdre_fb'
 closed_loop = False
 closed_loop = 'full_state_fb'
 closed_loop = None
-closed_loop = 'red_output_fb'
 closed_loop = 'hinf_red_output_fb'
+closed_loop = 'red_output_fb'
 # what inival
 whichinival = 'sstokes'  # steady state Stokes solution
 whichinival, tpp = 'sstokes++', .5  # a developed state starting from sstokes
