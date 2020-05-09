2011583ed8e7077726ed179e73fc9efb297e354c
Jan Heiland <jnhlnd@gmail.com>
2020-05-09 14:58:17 +0200

    script to include the deps

---
+++ tests/upd-deps.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+
+rsync -avC ~/software/gits/mypys/dolfin_navier_scipy/dolfin_navier_scipy/dolfin_to_sparrays.py ../dolfin_navier_scipy/
+rsync -avC ~/software/gits/mypys/dolfin_navier_scipy/dolfin_navier_scipy/data_output_utils.py ../dolfin_navier_scipy/
+
+rsync -avC ~/software/gits/mypys/sadptprj_riclyap_adi/sadptprj_riclyap_adi/lin_alg_utils.py ../sadptprj_riclyap_adi/
