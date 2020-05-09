24b066c45863dbddb072d93b137d92622288bc21
Jan Heiland <jnhlnd@gmail.com>
2020-05-09 15:18:29 +0200

    added the python paths

--- tests/addpypath.sh
+++ tests/addpypath.sh
@@ -1,2 +1,3 @@
 MYHOMEPATH=/home/heiland
+export PYTHONPATH="$PYTHONPATH:$MYPYPATH/spacetime_galerkin_pod:$MYPYPATH/dolfin_navier_scipy:$MYPYPATH/sadptprj_riclyap_adi:$MYPYPATH/distr_control_fenics:$MYPYPATH/mat_lib_plots"
 export PYTHONPATH="$PYTHONPATH:$MYHOMEPATH/work/code/lqgbt-oseen"
