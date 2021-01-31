52f0bcae340303cd9187a3cc947d8d73c0a1953c
Jan Heiland <jnhlnd@gmail.com>
2021-01-31 17:34:04 +0100

    upd setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,23 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(name='lqgbt_oseen',
-      version='v1.5',
+      version='2.0.0',
       description='LQGBT-controller for incompressible flows',
-      license="MIT",
+      license="GPLv3",
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Jan Heiland',
       author_email='jnhlnd@gmail.com',
       url="https://github.com/highlando/lqgbt-oseen",
       packages=['lqgbt_oseen'],  # same as name
-      install_requires=['numpy', 'scipy']  # external packages as dependencies
+      install_requires=['numpy', 'scipy',
+                        'dolfin_navier_scipy',
+                        'sadptprj_riclyap_adi',
+                        'distributed_control_fenics'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: OS Independent",
+          ]
+      # external packages as dependencies
       )
