ae089f3bab162ec40493104e5f9097fca7a12631
Jan Heiland <jnhlnd@gmail.com>
2021-01-31 20:27:23 +0100

    no more needed -- use `pip install -e .`

--- tests/addpypath.sh
+++
@@ -1,5 +0,0 @@
-MYHOMEPATH=/home/heiland
-export PYTHONPATH="$MYHOMEPATH/work/code/lqgbt-oseen"
-# MYPYPATH=/home/heiland/software/mypys
-# export PYTHONPATH="$PYTHONPATH:$MYPYPATH/dolfin_navier_scipy"
-# export PYTHONPATH="$PYTHONPATH:$MYPYPATH/sadptprj_riclyap_adi"
